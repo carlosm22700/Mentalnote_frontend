29e443c924c6f018e4ff59236499842bbdf75ba2
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 19:08:03 -0700

    renders SignUp Form

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,7 +1,10 @@
+import SignupForm from "../../components/SignupForm/SignupForm";
+
 export default function AuthPage() {
   return (
     <main>
       <h1>AuthPage</h1>
+      <SignupForm />
     </main>
   );
 }
