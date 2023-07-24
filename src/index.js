a6385458ab606473656c278307f01c621b80d598
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 18:11:04 -0700

    bugfix: fixes path to app file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./pages/App/App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter as Router } from "react-router-dom";
