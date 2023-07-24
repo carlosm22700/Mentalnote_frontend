f8b614a928fdcba5279d4ea6b2522f518608bd46
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 17:37:13 -0700

    implements mui default styles and tags

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
 
+const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
