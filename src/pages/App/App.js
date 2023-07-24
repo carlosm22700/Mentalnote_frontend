ceaeb80d28afe9c7a0984d75230fbd1d9dc0595c
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-23 18:11:52 -0700

    reformats App.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import AuthPage from "../AuthPage/AuthPage";
 
 function App() {
-  return <></>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Add your API call here to fetch the authenticated user if exists
+    // Upon getting the user set it in state.
+  }, []);
+
+  return (
+    <main className="App">
+      {user ? (
+        <>{/* Add your Route components here */}</>
+      ) : (
+        <Routes>
+          <Route path="/" element={<AuthPage setUser={setUser} />} />
+        </Routes>
+      )}
+    </main>
+  );
 }
 
 export default App;
